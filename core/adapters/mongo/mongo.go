ecfb172a0a98ea7fd7b2417e7e6c7b958a228d33
Shepherd <neiferjr15@gmail.com>
2025-06-21 02:40:21 -0500

    feat: eliminar URI de conexión a MongoDB para mayor seguridad

--- core/adapters/mongo/mongo.go
+++ core/adapters/mongo/mongo.go
@@ -14,7 +14,7 @@ func NewMongoConnection(config config.Config) *mongo.Client {
 
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://neiferjr15:test@cluster0.1munwg4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI("").SetServerAPIOptions(serverAPI)
 
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(opts)
