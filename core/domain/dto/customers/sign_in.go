e23b1f369dd0bc010eb69c8c04d0b30542f778de
Shepherd <neiferjr15@gmail.com>
2025-08-22 22:37:30 -0500

    feat: eliminar línea en la documentación Swagger y ajustar validación de contraseña en SignIn

--- core/domain/dto/customers/sign_in.go
+++ core/domain/dto/customers/sign_in.go
@@ -4,7 +4,7 @@ import "context"
 
 type SignIn struct {
 	PhoneNumber string `json:"phone_number" validate:"required,e164" example:"+573001112233"`
-	Password    string `json:"password" validate:"required,min=8" example:"claveSegura123"`
+	Password    string `json:"password" validate:"required" example:"claveSegura123"`
 }
 
 func (s *SignIn) Validate() error {
