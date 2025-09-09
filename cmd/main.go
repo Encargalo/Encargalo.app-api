ee312e6cf093a652de2855c795aeff7edc05b7f1
Shepherd <neiferjr15@gmail.com>
2025-09-09 11:59:43 -0500

    feat: eliminar comentario de prueba en la función main

--- cmd/main.go
+++ cmd/main.go
@@ -16,8 +16,6 @@ import (
 // @Schemes http
 func main() {
 
-	//Comentario de prueba
-
 	container := providers.BuildContainer()
 
 	if err := container.Invoke(func(router *router.Router, server *echo.Echo, config config.Config) {
